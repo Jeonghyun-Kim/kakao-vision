9c602e299885ac668f12d136929ad9e793eaa257
Kay Kim <kimjh@bawi.org>
2021-07-12 22:11:02 +0900

    add CDN_URL to .env.local.sample
    add https://kakao-vision.kay.kr to whitelist

--- lib/server/check-referrer.ts
+++ lib/server/check-referrer.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { createError } from '@defines/errors';
 
-const referrerWhitelsit = ['http://localhost:3000'];
+const referrerWhitelsit = ['http://localhost:3000', 'https://kakao-vision.kay.kr'];
 
 export function checkReferrer(req: NextApiRequest, res: NextApiResponse) {
   for (const referrer of referrerWhitelsit) {
