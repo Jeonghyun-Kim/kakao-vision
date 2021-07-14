67afb0462247fb0b62523af3d66729e3a0cc2eb3
Kay Kim <kimjh@bawi.org>
2021-07-14 17:49:26 +0900

    add cors check

--- lib/server/check-referrer.ts
+++ lib/server/check-referrer.ts
@@ -1,9 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors';
 import { createError } from '@defines/errors';
 
 const referrerWhitelsit = ['http://localhost:3000', 'https://kakao-vision.kay.kr'];
 
+const cors = Cors({ methods: ['GET', 'POST'], origin: '*' });
+
 export function checkReferrer(req: NextApiRequest, res: NextApiResponse) {
+  cors(req, res, (result) => {
+    if (result instanceof Error) {
+      throw createError('NO_PERMISSION');
+    }
+  });
+
   for (const referrer of referrerWhitelsit) {
     if (req.headers.referer?.includes(referrer)) {
       return;
