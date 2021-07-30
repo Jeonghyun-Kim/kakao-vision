bfc902d0a8f3d1d72dd802136bd55411599113b5
Kay Kim <kimjh@bawi.org>
2021-07-30 10:33:40 +0900

    cookie test

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -3,6 +3,7 @@ import { withErrorHandler } from '@utils/with-error-handler';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
+    res.setHeader('Set-Cookie', 'kay.test=hello-world; path=/; httpOnly; secure;');
     return res.json({ status: 'ok' });
   }
 };
