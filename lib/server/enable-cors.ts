ce5e8b52344943b8467881ec0d5339fa1bc3cda0
Kay Kim <kimjh@bawi.org>
2021-07-30 10:59:55 +0900

    add credentials

--- lib/server/enable-cors.ts
+++ lib/server/enable-cors.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import Cors from 'cors';
 import { runMiddleware } from '@utils/run-middleware';
 
-const cors = Cors({ origin: ['http://localhost:3000'] });
+const cors = Cors({ origin: ['http://localhost:3000'], credentials: true });
 
 export async function enableCors(req: NextApiRequest, res: NextApiResponse) {
   await runMiddleware(req, res, cors);
